027ddafc2eaeaa777fe06b1ccdf2c332d05d626a
Niurmiguel <niurmiguelg@gmail.com>
2019-08-12 17:00:37 -0400

    ....

--- src/pages/home/student.jsx
+++ src/pages/home/student.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
-import Input from '../../components/inputs/default'
+// import Input from '../../components/inputs/default'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
-import 'moment/locale/es'
+// import 'moment/locale/es'
 import { getRequests, getRequestForm, getRequestsCreated, setRequestCreated, deleteRequestCreated } from '../../_services'
 
-moment.locale('es')
+// moment.locale('es')
+
+const Input = (props) => {
+
+  return (
+    <div className="uk-margin">
+      <label className="uk-form-label" forhtml={ props.id }>{ props.label }</label>
+      <div className="uk-form-controls">
+        { props.type === 'BOOLEAN' ?
+          <div className="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+            <label><input className="uk-checkbox" name={ props.name } type="checkbox" value={true} onChange={ (e) => props.handleChange(e)}/> Si</label>
+            <label><input className="uk-checkbox" name={ props.name } type="checkbox" value={false} onChange={ (e) => props.handleChange(e)}/> No</label>
+          </div> :
+          <input
+            className="uk-input"
+            id={ props.id }
+            type={ props.type }
+            name={ props.name }
+            placeholder={ props.placeholder }
+            value={ props.value }
+            onChange={ (e) => props.handleChange(e) }
+          />
+        }
+      </div>
+    </div>
+  )
+}
+
 
 class StudentHome extends React.Component {
   constructor(props) {
